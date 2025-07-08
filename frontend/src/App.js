c2ec9ece6f93bda4875709c1087f6235431a1861
mancavesports3024 <mancavesportscardsllc@gmail.com>
2025-07-08 15:36:57 -0500

    Suppress exhaustive-deps warning for loadSearchHistory useEffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -151,6 +151,7 @@ function App() {
   };
 
   // Load search history on component mount
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   React.useEffect(() => {
     loadSearchHistory();
   }, []);
