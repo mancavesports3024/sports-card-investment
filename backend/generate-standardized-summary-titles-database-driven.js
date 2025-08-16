a2a40a55d433aff5757f3e12b085f89b18df4132
mancavesports3024 <mancavesportscardsllc@gmail.com>
2025-08-15 22:49:55 -0500

    Add targeted regex patterns to fix specific ALL CAPS player name issues

--- backend/generate-standardized-summary-titles-database-driven.js
+++ backend/generate-standardized-summary-titles-database-driven.js
@@ -596,6 +596,22 @@ class DatabaseDrivenStandardizedTitleGenerator {
         cleanTitle = cleanTitle.replace(/\bUSA\b/g, ''); // Remove USA
         cleanTitle = cleanTitle.replace(/\bLIGHT\b/g, ''); // Remove LIGHT
         
+        // Remove specific problematic patterns that are causing ALL CAPS player names
+        cleanTitle = cleanTitle.replace(/\bF1\b/g, ''); // Remove F1
+        cleanTitle = cleanTitle.replace(/\bHOF\b/g, ''); // Remove HOF
+        cleanTitle = cleanTitle.replace(/\bTWINS\b/g, ''); // Remove TWINS
+        cleanTitle = cleanTitle.replace(/\bTP\b/g, ''); // Remove TP
+        cleanTitle = cleanTitle.replace(/\bHB\b/g, ''); // Remove HB
+        cleanTitle = cleanTitle.replace(/\bCJ\b/g, ''); // Remove CJ
+        cleanTitle = cleanTitle.replace(/\bBA\b/g, ''); // Remove BA
+        cleanTitle = cleanTitle.replace(/\bLK\b/g, ''); // Remove LK
+        cleanTitle = cleanTitle.replace(/\bSTARCADE\b/g, ''); // Remove STARCADE
+        cleanTitle = cleanTitle.replace(/\bCOLLECTION\b/g, ''); // Remove COLLECTION
+        cleanTitle = cleanTitle.replace(/\bMETS\b/g, ''); // Remove METS
+        cleanTitle = cleanTitle.replace(/\bBREWERS\b/g, ''); // Remove BREWERS
+        cleanTitle = cleanTitle.replace(/\bFEVER\b/g, ''); // Remove FEVER
+        cleanTitle = cleanTitle.replace(/\bLONGHORNS\b/g, ''); // Remove LONGHORNS
+        
         // Remove emojis and special characters
         cleanTitle = cleanTitle.replace(/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]|[\u{1F900}-\u{1F9FF}]|[\u{1F018}-\u{1F270}]|[\u{238C}-\u{2454}]|[\u{20D0}-\u{20FF}]|[\u{FE00}-\u{FE0F}]|[\u{1FAB0}-\u{1FABF}]|[\u{1FAD0}-\u{1FAFF}]|[\u{1FA70}-\u{1FAFF}]|[\u{1FA00}-\u{1FA6F}]|[\u{1F9D0}-\u{1F9FF}]|[\u{1F910}-\u{1F93A}]|[\u{1F950}-\u{1F9C0}]|[\u{1F004}]|[\u{1F0CF}]|[\u{1F170}-\u{1F251}]|[\u{2139}]|[\u{2194}-\u{2199}]|[\u{21A9}-\u{21AA}]|[\u{231A}-\u{231B}]|[\u{23E9}-\u{23EC}]|[\u{23F0}]|[\u{23F3}]|[\u{25AA}-\u{25AB}]|[\u{25B6}]|[\u{25C0}]|[\u{25FB}-\u{25FE}]|[\u{2600}-\u{2604}]|[\u{2614}-\u{2615}]|[\u{2648}-\u{2653}]|[\u{26AA}-\u{26AB}]|[\u{26BD}-\u{26BE}]|[\u{26C4}-\u{26C5}]|[\u{26CE}]|[\u{26D4}]|[\u{26EA}]|[\u{26F2}-\u{26F3}]|[\u{26F5}]|[\u{26FA}]|[\u{26FD}]|[\u{2702}]|[\u{2705}]|[\u{2708}-\u{270C}]|[\u{270D}]|[\u{270F}]|[\u{2712}]|[\u{2714}]|[\u{2716}]|[\u{271D}]|[\u{2721}]|[\u{2728}]|[\u{2733}-\u{2734}]|[\u{2744}]|[\u{2747}]|[\u{274C}]|[\u{274E}]|[\u{2753}-\u{2755}]|[\u{2757}]|[\u{2763}-\u{2764}]|[\u{2795}-\u{2797}]|[\u{27A1}]|[\u{27B0}]|[\u{27BF}]|[\u{2934}-\u{2935}]|[\u{2B05}-\u{2B07}]|[\u{2B1B}-\u{2B1C}]|[\u{2B50}]|[\u{2B55}]|[\u{3030}]|[\u{303D}]|[\u{3297}]|[\u{3299}]|[\u{1F004}]|[\u{1F0CF}]|[\u{1F170}-\u{1F251}]|[\u{2139}]|[\u{2194}-\u{2199}]|[\u{21A9}-\u{21AA}]|[\u{231A}-\u{231B}]|[\u{23E9}-\u{23EC}]|[\u{23F0}]|[\u{23F3}]|[\u{25AA}-\u{25AB}]|[\u{25B6}]|[\u{25C0}]|[\u{25FB}-\u{25FE}]|[\u{2600}-\u{2604}]|[\u{2614}-\u{2615}]|[\u{2648}-\u{2653}]|[\u{26AA}-\u{26AB}]|[\u{26BD}-\u{26BE}]|[\u{26C4}-\u{26C5}]|[\u{26CE}]|[\u{26D4}]|[\u{26EA}]|[\u{26F2}-\u{26F3}]|[\u{26F5}]|[\u{26FA}]|[\u{26FD}]|[\u{2702}]|[\u{2705}]|[\u{2708}-\u{270C}]|[\u{270D}]|[\u{270F}]|[\u{2712}]|[\u{2714}]|[\u{2716}]|[\u{271D}]|[\u{2721}]|[\u{2728}]|[\u{2733}-\u{2734}]|[\u{2744}]|[\u{2747}]|[\u{274C}]|[\u{274E}]|[\u{2753}-\u{2755}]|[\u{2757}]|[\u{2763}-\u{2764}]|[\u{2795}-\u{2797}]|[\u{27A1}]|[\u{27B0}]|[\u{27BF}]|[\u{2934}-\u{2935}]|[\u{2B05}-\u{2B07}]|[\u{2B1B}-\u{2B1C}]|[\u{2B50}]|[\u{2B55}]|[\u{3030}]|[\u{303D}]|[\u{3297}]|[\u{3299}]/gu, '');
         
